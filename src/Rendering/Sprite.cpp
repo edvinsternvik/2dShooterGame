b1c3052e4608c1eb9cb124863d45edaf72582958
RedE01 <e.sternvik@gmail.com>
2022-02-20 11:55:54 +0100

    Improve sprite rendering

--- src/Rendering/Sprite.cpp
+++ src/Rendering/Sprite.cpp
@@ -4,37 +4,47 @@
 #include <memory>
 #include <GL/glew.h>
 
+#define VIEWPORT_WIDTH 320
+#define VIEWPORT_HEIGHT 180
+
 static std::unique_ptr<SpriteData> spriteData;
 
-Sprite::Sprite() {
+Sprite::Sprite(std::shared_ptr<Texture> spriteTexture)
+    : m_spriteTexture(spriteTexture) {
     if(spriteData.get() == nullptr) spriteData = std::make_unique<SpriteData>();
 }
 
-void Sprite::render(glm::vec2 pos, float scale, float angle, Shader* shader) {
+void Sprite::render(glm::vec2 pos, float scale, float angle, Shader* shader) const {
     spriteData->vao.bind();
+
+    glm::vec2 spriteScale(
+        scale * m_spriteTexture->getWidth() / static_cast<float>(VIEWPORT_WIDTH),
+        scale * m_spriteTexture->getHeight() / static_cast<float>(VIEWPORT_HEIGHT)
+    );
 
     if(angle == 0) {
         glm::mat3 transformMatrix(
-            scale, 0.0, pos.x,
-            0.0, scale, pos.y,
-            0.0, 0.0, scale
+            spriteScale.x, 0.0,           pos.x,
+            0.0,           spriteScale.y, pos.y,
+            0.0,           0.0,           1.0
         );
         shader->setUniformMat3("u_transformMatrix", transformMatrix);
     }
     else {
         glm::mat3 transformMatrix(
-            scale * std::cos(angle), scale * -std::sin(angle), pos.x,
-            scale * std::sin(angle), scale *  std::cos(angle), pos.y,
-            0.0,             0.0,              1.0
+            spriteScale.x * std::cos(angle), spriteScale.x * -std::sin(angle), pos.x,
+            spriteScale.y * std::sin(angle), spriteScale.y *  std::cos(angle), pos.y,
+            0.0,                              0.0,                             1.0
         );
         shader->setUniformMat3("u_transformMatrix", transformMatrix);
     }
 
+    m_spriteTexture->bind();
     glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
 
     spriteData->vao.unbind();
 }
 
-void Sprite::render(glm::vec2 pos, float scale, Shader* shader) {
+void Sprite::render(glm::vec2 pos, float scale, Shader* shader) const {
     render(pos, scale, 0.0, shader);
 }
--- src/Rendering/Sprite.h
+++ src/Rendering/Sprite.h
@@ -1,12 +1,17 @@
 #pragma once
 #include <glm/glm.hpp>
+#include "Texture.h"
+#include <memory>
 
 class Shader;
 
 class Sprite {
 public:
-    Sprite();
+    Sprite(std::shared_ptr<Texture> spriteTexture);
 
-    void render(glm::vec2 pos, float scale, float angle, Shader* shader);
-    void render(glm::vec2 pos, float scale, Shader* shader);
+    void render(glm::vec2 pos, float scale, float angle, Shader* shader) const;
+    void render(glm::vec2 pos, float scale, Shader* shader) const;
+
+private:
+    std::shared_ptr<Texture> m_spriteTexture;
 };
